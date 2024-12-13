16734798382508607f77d240d5faf6b7f77ed11f
kwaak <kwaak@bosun.ai>
2024-12-13 11:21:08 +0000

    [kwaak]: Committed changes after completion

--- src/util.rs
+++ src/util.rs
@@ -21,6 +21,7 @@ pub fn accept_non_zero_exit(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use swiftide::traits::{CommandError, CommandOutput};
 
     #[test]
     fn test_strip_markdown_tags() {
@@ -55,5 +56,22 @@ mod tests {
         ```";
         let expected = "This is a test.";
         assert_eq!(strip_markdown_tags(input), expected);
+    }
+
+    #[test]
+    fn test_accept_non_zero_exit() {
+        // Case: Result is Ok(CommandOutput)
+        let output = CommandOutput::new(vec!["Output Line 1".into(), "Output Line 2".into()]);
+        let result = Ok(output.clone());
+        assert_eq!(accept_non_zero_exit(result), Ok(output));
+        
+        // Case: Result is Err(CommandError::FailedWithOutput(CommandOutput))
+        let err_result = Err(CommandError::FailedWithOutput(output.clone()));
+        assert_eq!(accept_non_zero_exit(err_result), Ok(output));
+
+        // Case: Result is Err(CommandError::Other)
+        let other_error = CommandError::Other("Unknown Error".into());
+        let err_result = Err(other_error.clone());
+        assert_eq!(accept_non_zero_exit(err_result), Err(other_error));
     }
 }
