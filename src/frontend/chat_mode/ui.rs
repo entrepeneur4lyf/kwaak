85587752cd7908f33a74bdec4cb3fee832caaf39
kwaak <kwaak@bosun.ai>
2025-01-06 13:05:13 +0000

    [kwaak]: Committed changes after completion

--- src/frontend/chat_mode/ui.rs
+++ src/frontend/chat_mode/ui.rs
@@ -113,4 +113,136 @@ fn render_chat_messages(f: &mut ratatui::Frame, app: &mut App, area: Rect) {
     // Render scrollbar
     f.render_stateful_widget(
         Scrollbar::new(ScrollbarOrientation::VerticalRight)
-            .begin_symbol(Some("\
+            .begin_symbol(Some("\u2191"))
+            .end_symbol(Some("\u2193")),
+        area,
+        &mut current_chat.vertical_scroll_state,
+    );
+}
+
+fn render_chat_list(f: &mut ratatui::Frame, app: &mut App, area: Rect) {
+    let list: List = app
+        .chats
+        .iter()
+        .map(format_chat_in_list)
+        .collect::<List>()
+        .highlight_spacing(HighlightSpacing::Always)
+        .highlight_style(Style::default().fg(Color::Yellow).bg(Color::DarkGray))
+        .block(
+            Block::default()
+                .title("Chats".bold())
+                .title_alignment(Alignment::Center)
+                .borders(Borders::TOP | Borders::RIGHT)
+                .padding(Padding::right(1)),
+        );
+
+    f.render_stateful_widget(list, area, &mut app.chats_state);
+}
+
+fn format_chat_in_list(chat: &Chat) -> ListItem {
+    let suffix = if chat.is_loading() { " ..." } else { "" };
+
+    let new_message_count = if chat.new_message_count > 0 {
+        format!(" ({})", chat.new_message_count)
+    } else {
+        String::new()
+    };
+
+    ListItem::from(format!(
+        "{name}{suffix}{new_message_count}",
+        name = chat.name
+    ))
+}
+
+fn render_input_bar(f: &mut ratatui::Frame, app: &mut App, area: Rect) {
+    let border_set = symbols::border::Set {
+        top_left: symbols::line::NORMAL.vertical_right,
+        top_right: symbols::line::NORMAL.vertical_left,
+        bottom_right: symbols::line::NORMAL.horizontal_up,
+        ..symbols::border::PLAIN
+    };
+
+    let block = Block::default()
+        .border_set(border_set)
+        .padding(Padding::horizontal(1))
+        .borders(Borders::ALL);
+
+    if app.current_chat().is_some_and(Chat::is_loading) {
+        let loading_msg = match &app.current_chat().expect("infallible").state {
+            ChatState::Loading => "Kwaaking ...".to_string(),
+            ChatState::LoadingWithMessage(msg) => format!("Kwaaking ({msg}) ..."),
+            ChatState::Ready => unreachable!(),
+        };
+        let throbber = throbber_widgets_tui::Throbber::default().label(&loading_msg);
+
+        f.render_widget(throbber, block.inner(area));
+        return block.render(area, f.buffer_mut());
+    }
+
+    // let input = Paragraph::new(app.input.as_str()).block(block);
+    app.text_input.set_block(block);
+    f.render_widget(&app.text_input, area);
+    // Set cursor position
+    // f.set_cursor_position(
+    //     // Put cursor past the end of the input text
+    //     #[allow(clippy::cast_possible_truncation)]
+    //     (area.x + app.input.len() as u16 + 1, area.y + 1),
+    // );
+}
+
+fn render_help(f: &mut ratatui::Frame, app: &App, area: Rect) {
+    let border_set = symbols::border::Set {
+        top_right: symbols::line::NORMAL.vertical_left,
+        ..symbols::border::PLAIN
+    };
+    let [top, bottom] = Layout::vertical([
+        #[allow(clippy::cast_possible_truncation)]
+        Constraint::Length(app.supported_commands().len() as u16 + 3),
+        Constraint::Min(4),
+    ])
+    .areas(area);
+
+    Paragraph::new(
+        app.supported_commands()
+            .iter()
+            .map(|c| Line::from(format!("/{c}").bold()))
+            .collect::<Vec<Line>>(),
+    )
+    .block(
+        Block::default()
+            .title("Chat commands".bold())
+            .title_alignment(Alignment::Center)
+            .borders(Borders::TOP | Borders::RIGHT)
+            .border_set(border_set)
+            .padding(Padding::uniform(1)),
+    )
+    .render(top, f.buffer_mut());
+
+    let border_set = symbols::border::Set {
+        top_right: symbols::line::NORMAL.vertical_left,
+        ..symbols::border::PLAIN
+    };
+    Paragraph::new(
+        [
+            "Page Up/Down - Scroll",
+            "End - Scroll to end",
+            "^s - Send message",
+            "^s - Send message",
+            "^x - Stop agent",
+            "^n - New chat",
+            "^c - Quit",
+        ]
+        .iter()
+        .map(|h| Line::from(h.bold()))
+        .collect::<Vec<Line>>(),
+    )
+    .block(
+        Block::default()
+            .title("Keybindings".bold())
+            .title_alignment(Alignment::Center)
+            .border_set(border_set)
+            .borders(Borders::TOP | Borders::RIGHT | Borders::BOTTOM)
+            .padding(Padding::uniform(1)),
+    )
+    .render(bottom, f.buffer_mut());
+}
