ab5be745fcf9fc2eaf9dd886ae00d32ccfd98edf
kwaak <kwaak@bosun.ai>
2025-01-06 15:33:45 +0000

    [kwaak]: Committed changes after completion

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,5 +1,7 @@
 extern crate tempfile;
 
+use crate::config::Config;
+use crate::repository::Repository;
 use std::path::PathBuf;
 use tempfile::TempDir;
 
@@ -7,11 +9,11 @@ pub struct TestGuard {
     pub tempdir: TempDir,
 }
 
-pub fn test_repository(config: &crate::config::Config) -> (crate::git::Repository, TestGuard) {
+pub fn test_repository(config: &Config) -> (Repository, TestGuard) {
     let tempdir = TempDir::new().expect("Failed to create tempdir");
     let repo_path = tempdir.path().join("repo");
     std::fs::create_dir(&repo_path).expect("Failed to create repo directory");
-    let repository = crate::git::Repository::from_config(config, &repo_path)
-        .expect("Failed to create test repository");
+    let repository =
+        Repository::from_config(config.into()).expect("Failed to create test repository");
     (repository, TestGuard { tempdir })
 }
