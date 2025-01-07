70bb88c18640aafb5188391b4e51e12b0829dd38
kwaak <kwaak@bosun.ai>
2025-01-07 09:31:37 +0000

    [kwaak]: Committed changes after completion

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
+mod agent;
 mod chat;
+mod chat_message;
 mod commands;
 mod frontend;
+mod indexing;
+mod repository;
 
+pub use agent::*;
 pub use chat::*;
+pub use chat_message::*;
 pub use commands::*;
 pub use frontend::*;
+pub use indexing::*;
+pub use repository::*;
