97bfc580fa91b9e00f9ad8b138cfd61dfd43a8fe
kwaak <kwaak@bosun.ai>
2025-01-27 16:00:14 +0000

    [kwaak]: Committed changes after completion

--- src/frontend/app.rs
+++ src/frontend/app.rs
@@ -396,7 +396,7 @@ impl App<'_> {
                 current_chat.vertical_scroll = current_chat.vertical_scroll.saturating_sub(2);
                 current_chat.vertical_scroll_state = current_chat
                     .vertical_scroll_state
-                    .position(current_chat.vertical_scroll);
+                current_chat.auto_tailing_enabled = false;
             }
             UIEvent::ScrollDown => {
                 let Some(current_chat) = self.current_chat_mut() else {
