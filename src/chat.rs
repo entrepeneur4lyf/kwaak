70ce4a2ec1f377ac01c01827009b2018f672458a
kwaak <kwaak@bosun.ai>
2025-01-27 16:00:01 +0000

    [kwaak]: Committed changes after completion

--- src/chat.rs
+++ src/chat.rs
@@ -20,7 +20,7 @@ pub struct Chat {
     // but handled in the ui
     pub vertical_scroll_state: ScrollbarState,
     pub vertical_scroll: usize,
-    pub num_lines: usize,
+    pub auto_tailing_enabled: bool,
 }
 
 impl Chat {
